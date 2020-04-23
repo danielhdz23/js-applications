aee7d21ed9b968c09a98226f7bc535e09adf01b6
Daniel H. L <danielhdz.ingeniero@gmail.com>
2020-04-22 19:23:49 -0500

    'commit'

--- covid-19-stats/js/component.js
+++ covid-19-stats/js/component.js
@@ -14,6 +14,7 @@ fetch("https://www.datos.gov.co/resource/gt2j-8ykr.json?$limit=5000", {
   .then((myJson) => {
     datosCoronavirus = myJson;
     mostrarDatos(myJson);
+    dibujarGraficas(myJson)
   })
   .catch((err) => {
     console.log(err);
@@ -60,6 +61,7 @@ let mostrarDatos = function (datosCoronavirus) {
   });
 };
 
+
 let calcularProbabilidad = function () {
   let persona = {
     name: "",
@@ -102,14 +104,137 @@ let calcularProbabilidad = function () {
       persona.cares.house_cleaning = el.value;
     }
   });
+
+  /*
+  Prob contagio
+*/
+
+let contagio;
+let casosDepartamento = datosCoronavirus.filter(({departamento})=>persona.departament === departamento)
+
+contagio = (casosDepartamento.length / datosCoronavirus.length) * 100
+
+/********/
+
   console.log(persona);
   persona.probability =
-    (0.5 +
+    (contagio +
       parseFloat(persona.times_out) -
       parseFloat(persona.cares.mask) -
       parseFloat(persona.cares.infection_measures) -
       parseFloat(persona.cares.house_cleaning)) *
     100;
 
-  document.getElementById("mensaje").append(Math.round(persona.probability));
+  document.getElementById("mensaje").innerText =(Math.round(persona.probability));
 };
+
+/*
+let contagio;
+let casosDepartamento = datosCoronavirus.filter(({departamento})=>persona.departamento === departamento)
+
+contagio = (casosDepartamento.length / datosCoronavirus.length) * 100
+
+
+
+switch (persona.salidas) {
+  case 'ninguna':
+    break;
+  case '1-3':
+    contagio *= 4
+    break;
+  case '4-10':
+    contagio *= 8
+    break;
+  case '10+':
+    contagio *= 16
+    break;
+  default:
+    break;
+}
+console.log(contagio)
+
+*/
+
+
+let dibujarGraficas = function(){
+    let fechasContagio = datosCoronavirus.map(({fecha_diagnostico}) => fecha_diagnostico)
+
+    let aumentoxdia = []
+    let indice = 0
+    let dia = 0
+    let contagios = 0
+    fechasContagio.forEach(
+      fecha => {
+        if(fechasContagio.indexOf(fecha)===indice){
+          dia++
+          contagios++
+          aumentoxdia[dia] = contagios
+        }else {
+          contagios++
+          aumentoxdia[dia] = contagios
+        }
+        indice++
+      }
+    )
+
+    new Chartist.Line('.ct-chart', {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      series: [
+       aumentoxdia
+      ]
+    }, {
+      fullWidth: true,
+      chartPadding: {
+        right: 40
+      }
+    });
+  };
+/****************Graficas PAÍSES**************/
+  let datosPaisesCovid;
+  fetch("https://api.covid19api.com/country/italy?from=2020-03-01T00:00:00Z&to=2020-04-01T00:00:00Z", {
+  method: "GET",
+  })
+  .then((respuesta) => {
+    return respuesta.json();
+  })
+  .then((myJson) => {
+    datosPaisesCovid = myJson;
+    dibujarGraficasPaises(myJson)
+    console.log(datosPaisesCovid)
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  let dibujarGraficasPaises = function(){
+    let fechasContagio = datosPaisesCovid.map(({Confirmed}) => Confirmed)
+    let aumentoxdia = []
+    let indice = 0
+    let dia = 0
+    let contagios = 0
+    fechasContagio.forEach(
+      Date => {
+        if(fechasContagio.indexOf(Date)===indice){
+          dia++
+          contagios++
+          aumentoxdia[dia] = contagios
+        }
+        indice++
+      }
+    )
+
+    console.log(aumentoxdia)
+    console.log(contagios)
+
+    new Chartist.Line('.ct-chart-2', {
+      labels: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'],
+      series: [
+       fechasContagio
+      ]
+    }, {
+      fullWidth: true,
+      chartPadding: {
+        right: 40
+      }
+    });
+  }
